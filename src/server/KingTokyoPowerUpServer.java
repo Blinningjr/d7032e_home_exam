51bb0d8de08e8fbdcc5449149465912ced39e73f
Blinningjr <nicke.l@telia.com>
2019-10-24 13:10:51 +0200

    File structure

---
+++ src/server/KingTokyoPowerUpServer.java
@@ -0,0 +1,14 @@
+
+
+public class KingTokyoPowerUpServer {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        // https://www.youtube.com/watch?v=HqdOaAzPtek
+        // https://boardgamegeek.com/thread/1408893/categorizing-cards
+        new KingTokyoPowerUpServer();
+    }
+    
+}
