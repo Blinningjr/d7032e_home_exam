dfb20975cd6d0b436d145cd44fcc78f7884d0703
Blinningjr <nicke.l@telia.com>
2019-10-24 17:56:15 +0200

    Implemented temporery effect implementation

--- src/server/model/cards/Effect.java
+++ src/server/model/cards/Effect.java
@@ -1,5 +1,11 @@
 package server.model.cards;
 
-public class Efffect {
 
+public class Efffect {
+    // OPS! This is the teachers implementation of effect. Change later.
+    public int moreDamage = 0; //Acid Attack
+    public int cardsCostLess = 0; //Alien Metabolism
+    public int starsWhenAttacking = 0; //Alpha monster
+    public int stars = 0; //Apartment Building, Commuter Train, Corner Stone
+    public int armor = 0; //Armor Plating
 }
